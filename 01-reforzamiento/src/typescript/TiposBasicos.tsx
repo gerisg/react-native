79db492b5a45d7f6c9b5fa454b490562328af626
Gerardo Gallardo <ggallardo@asanteit.com.ar>
2021-10-23 18:14:02 -0300

    01 review

--- 01-reforzamiento/src/typescript/TiposBasicos.tsx
+++ 01-reforzamiento/src/typescript/TiposBasicos.tsx
@@ -1,17 +1,17 @@
 export const TiposBasicos = () => {
 
-    const nombre: string = 'Gerardo';
-    const edad: number = 37;
-    const estaActivo: boolean = false;
+  const nombre: string = 'Gerardo';
+  const edad: number = 37;
+  const estaActivo: boolean = false;
 
-    const poderes: string[] = ['Velocidad', 'Volar', 'Respirar en el agua'];
+  const poderes: string[] = ['Velocidad', 'Volar', 'Respirar en el agua'];
 
-    return (
-        <>
-            <h3>Tipos básicos</h3>
-            { nombre}, { edad}, { estaActivo ? 'activo' : 'no activo'}
-            <br />
-            { poderes.join(', ')}
-        </>
-    )
+  return (
+    <>
+      <h3>Tipos básicos</h3>
+      { nombre}, { edad}, { estaActivo ? 'activo' : 'no activo'}
+      <br />
+      { poderes.join(', ')}
+    </>
+  )
 }

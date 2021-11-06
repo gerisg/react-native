2da2d8e655b4424381219c2a76790db77cd11e74
Gerardo Gallardo <ggallardo@asanteit.com.ar>
2021-11-06 20:42:51 -0300

    06 Project clean with vectoricons and navigation

---
+++ 06-RNComponents/src/screens/HomeScreen.tsx
@@ -0,0 +1,12 @@
+import React from 'react';
+import { Text, View } from 'react-native';
+import Icon from 'react-native-vector-icons/Ionicons';
+
+export const HomeScreen = () => {
+  return (
+    <View>
+      <Text>HomeScreen</Text>
+      <Icon name="airplane" size={30} color={'red'} />
+    </View>
+  );
+};

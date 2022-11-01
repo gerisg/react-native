0c5bfa3ac289d9b57c76c57a25b0a0acb6ac14dd
Gerardo Gallardo <ggallardo@asanteit.com.ar>
2022-11-01 09:37:49 -0300

    06 Update

--- 06-RNComponents/src/screens/HomeScreen.tsx
+++ 06-RNComponents/src/screens/HomeScreen.tsx
@@ -1,12 +1,61 @@
 import React from 'react';
-import { Text, View } from 'react-native';
+import { FlatList, Text, View } from 'react-native';
+import { useSafeAreaInsets } from 'react-native-safe-area-context';
 import Icon from 'react-native-vector-icons/Ionicons';
+import styles from '../theme/appTheme';
+
+interface MenuItem {
+  name: string;
+  icon: string;
+  components: string;
+}
+
+const menuItems = [
+  {
+    name: 'Animation 101',
+    icon: 'cube-outline',
+    components: 'Animation101Screen',
+  },
+  {
+    name: 'Animation 102',
+    icon: 'albums-outline',
+    components: 'Animation102Screen',
+  },
+];
 
 export const HomeScreen = () => {
+  const { top } = useSafeAreaInsets();
+
+  const renderMenuItem = ({ name, icon }: MenuItem) => {
+    return (
+      <View>
+        <Text>{name}</Text>
+        <Icon name={icon} size={30} color={'red'} />
+      </View>
+    );
+  };
+
+  const renderListHeader = () => {
+    return (
+      <View style={{ marginTop: top + 20, marginBottom: 20 }}>
+        <Text style={styles.title}>Opciones de Menú</Text>
+      </View>
+    );
+  };
+
+  const itemSeparator = () => {
+    return <View style={styles.separator} />;
+  };
+
   return (
-    <View>
-      <Text>HomeScreen</Text>
-      <Icon name="airplane" size={30} color={'red'} />
+    <View style={{ flex: 1, ...styles.globalMargin }}>
+      <FlatList
+        data={menuItems}
+        renderItem={({ item }) => renderMenuItem(item)}
+        keyExtractor={item => item.name}
+        ListHeaderComponent={renderListHeader}
+        ItemSeparatorComponent={itemSeparator}
+      />
     </View>
   );
 };
---
+++ 06-RNComponents/src/theme/appTheme.tsx
@@ -0,0 +1,18 @@
+import { StyleSheet } from 'react-native';
+
+const styles = StyleSheet.create({
+  globalMargin: {
+    marginHorizontal: 20,
+  },
+  title: {
+    fontSize: 35,
+    fontWeight: 'bold',
+  },
+  separator: {
+    borderBottomWidth: 1,
+    opacity: 0.4,
+    marginVertical: 8,
+  },
+});
+
+export default styles;
